d4dae9f5fc3a6e5605a8177f9f3da18182cc27d9
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-12 13:45:34 +0300

    Get and install redux(logger/thunk)

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,11 @@
-import { combineReducers } from 'react-redux';
+import { combineReducers, createStore, applyMiddleware } from 'react-redux';
 import books from './books/books';
 import categories from './categories/categories';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
 
 const root = combineReducers({ books, categories });
+const store = createStore( root, applyMiddleware(logger, thunk), );
 
-export default root;
+export default store;
