3d6119a20813834c1346452bf7b3c7bb0c8b7810
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-12 18:14:48 +0300

    [Required Changes] use setState for the input onChange

--- src/components/RegisterBook.js
+++ src/components/RegisterBook.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 const RegisterBook = () => {
   const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const newBook = {
+    title,
+    author,
+  };
 
   const registerBook = (e) => {
     e.preventDefault();
-    const { title, author } = e.target.elements;
-    const newBook = {
-      title: title.value,
-      author: author.value,
-    };
-
     dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
   };
 
   return (
     <>
       <h2>Register Book</h2>
       <form className="register-book-form" onSubmit={registerBook}>
-        <input type="text" id="title" placeholder="Title" required />
-        <input type="text" id="author" placeholder="Author" required />
+        <input type="text" name="title" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+        <input type="text" name="author" id="author" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} required />
         <button type="submit">Register</button>
       </form>
     </>
