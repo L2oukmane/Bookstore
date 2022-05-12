bda0716ec6cb590991c398b2fe2cb46168a2d8ad
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-12 14:35:29 +0300

    Implement deleteHandler method

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,3 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
+
+const Book = (props) => {
+  const { id, title, author } = props;
+  const dispatch = useDispatch();
+
+  const deleteHandler = () => {
+    dispatch(removeBook(id));
+  };
+
+  return (
+    <li key={id}>
+      <p>{title}</p>
+      <p>{author}</p>
+      <button type="button" onClick={deleteHandler}>Remove</button>
+    </li>
+  );
+};
+
+Book.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+export default Book;
