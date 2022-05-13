155f4455f7b7d3607daa6e93b35689b972cba56c
Lucas Shangrita <oustanilokman@gmail.com>
2022-05-13 11:34:43 +0300

    Implement book registration action

--- src/components/RegisterBook.js
+++ src/components/RegisterBook.js
@@ -1,29 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/books';
+import { v4 as uuidv4 } from 'uuid';
+import { registerNewBook } from '../redux/books/books';
 
 const RegisterBook = () => {
   const dispatch = useDispatch();
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const newBook = {
-    title,
-    author,
-  };
 
   const registerBook = (e) => {
     e.preventDefault();
-    dispatch(addBook(newBook));
-    setTitle('');
-    setAuthor('');
+    const { title, author, category } = e.target.elements;
+    const newBook = {
+      item_id: uuidv4(),
+      title: title.value,
+      author: author.value,
+      category: category.value,
+    };
+
+    dispatch(registerNewBook(newBook));
+    title.value = '';
+    author.value = '';
+    category.value = '';
   };
 
   return (
     <>
       <h2>Register Book</h2>
       <form className="register-book-form" onSubmit={registerBook}>
-        <input type="text" name="title" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
-        <input type="text" name="author" id="author" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} required />
+        <input type="text" id="title" placeholder="Title" required />
+        <input type="text" id="author" placeholder="Author" required />
+        <input type="text" id="category" placeholder="Category" required />
         <button type="submit">Register</button>
       </form>
     </>
